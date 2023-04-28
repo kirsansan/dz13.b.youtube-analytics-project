23a893414f5810165befa48295521e666fb53086
kirsansan <kirill_home@mail.ru>
2023-04-28 22:10:26 +0300

    fix PlayList class for .__content_info and .__content_items

--- src/playlist.py
+++ src/playlist.py
@@ -5,61 +5,51 @@ from src.video import Video
 import isodate
 
 
-
 class PlayList(YouTubeConnectorMixin):
     def __init__(self, id_playlist):
         self.__total_duration = None
         self.id_playlist = id_playlist
-        self.__connector = self.get_connector()
-        self.__content = self.get_info()
-        self.__info = None
-        self.__is_connected = False
         self.title = None
         self.url = None
-        if self.__content:
-            self.parse_info()
-        else:
-            pass
-
+        self.__info = None
+        self.__connector = self.get_connector()
+        self.__content_info = self.get_info()
+        self.__content_items = self.get_items()
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.id_playlist}"
+        return f"{self.__class__.__name__}('{self.id_playlist}')"
 
     def __str__(self):
         return f"{self.title}"
 
-    def get_info(self):
+    def get_items(self):
         youtube = self.__connector
         playlist_videos = youtube.playlistItems().list(playlistId=self.id_playlist,
                                                        part='contentDetails, snippet',
                                                        maxResults=50,
                                                        ).execute()
-        self.__content = playlist_videos
+        self.__content_items = playlist_videos
         return playlist_videos
+
+    def get_info(self):
+        youtube = self.__connector
+        playlist_info = youtube.playlists().list(id=self.id_playlist,
+                                                 part='snippet'
+                                                 ).execute()
+        self.__content_info = playlist_info
+        self.__info = json.dumps(self.__content_info, indent=2, ensure_ascii=False)
+        self.title = self.__content_info["items"][0]["snippet"]["title"]
+        self.url = "https://www.youtube.com/playlist?list=" + self.id_playlist
+        return playlist_info
 
     def print_info(self):
         """ just touch set_info and print self.info"""
-        if self.__content:
+        if self.__content_info:
             print(self.__info)
-        else:
+        if self.__content_items:
+            print(self.__content_items)
+        if not self.__content_info and not self.__content_items:
             print("Not connected or parse error")
-
-
-    def parse_info(self):
-        """ set self.info as json and parse it"""
-        if self.__content:
-             self.__info = json.dumps(self.__content, indent=2, ensure_ascii=False)
-             self.title = self.__content["items"][0]["snippet"]["title"]
-             self.url = "https://www.youtube.com/playlist?list=" + self.id_playlist
-        #     self.title_playlist = self.content["items"][0]["snippet"]["title"]
-        #     self.url_video = self.content["items"][0]["snippet"]["thumbnails"]["default"]["url"]
-        #     tmp_likes_video: str = self.content["items"][0]["statistics"]["likeCount"]
-        #     if tmp_likes_video.isdigit():
-        #         self.likes_video = int(tmp_likes_video)
-        #     tmp_video_count: str = self.content["items"][0]["statistics"]["viewCount"]
-        #     if tmp_video_count.isdigit():
-        #         self.view_count_video = int(tmp_video_count)
-        pass
 
     @property
     def total_duration(self) -> datetime.timedelta:
@@ -68,7 +58,7 @@ class PlayList(YouTubeConnectorMixin):
         :return: datetime.timedelta object
         """
         tmp_time_summary = datetime.timedelta(seconds=0)
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__content['items']]
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__content_items['items']]
         for vid_id in video_ids:
             video = Video(vid_id)
             iso_8601_duration = video.get_duration()
@@ -81,7 +71,7 @@ class PlayList(YouTubeConnectorMixin):
         :return: url video"""
         max_video_time = 0
         video_link = None
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__content['items']]
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__content_items['items']]
         for vid_id in video_ids:
             video = Video(vid_id)
             if video.count_views > max_video_time:
@@ -89,11 +79,12 @@ class PlayList(YouTubeConnectorMixin):
                 max_video_time = video.count_views
         return video_link
 
+
 if __name__ == '__main__':
     pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
     # print(pl)
     # pl.print_info()
-    #print(pl.total_duration())
+    print(pl.total_duration)
     print("tdur=", pl.total_duration)
     print(type(pl.total_duration))
     print(pl.show_best_video())
